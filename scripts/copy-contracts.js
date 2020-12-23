fc8ddaa1a3e09f6627823e115b439f0f6a87d769
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-12-23 21:46:05 +0300

    v1.0.2

--- scripts/copy-contracts.js
+++ scripts/copy-contracts.js
@@ -12,10 +12,10 @@ main()
 async function main() {
   return new Promise((resolve, reject) => {
     const npx = execSync(
-      'npx copyfiles -f ./artifacts/contracts/**/*.json ./build/contracts && del-cli ./build/contracts/*.dbg.json &&'+
-      'npx copyfiles -f ./artifacts/contracts/interfaces/**/*.json ./build/contracts/interfaces && del-cli ./build/contracts/interfaces/*.dbg.json &&' +
-      'npx copyfiles -f ./artifacts/contracts/libraries/**/*.json ./build/contracts/libraries && del-cli ./build/contracts/libraries/*.dbg.json &&' +
-      'npx copyfiles -f copyfiles -f ./artifacts/contracts/test/**/*.json ./build/contracts/test && del-cli ./build/contracts/test/*.dbg.json'
+      'npx copyfiles -f ./artifacts/contracts/**/*.json ./build && del-cli ./build/*.dbg.json &&'+
+      'npx copyfiles -f ./artifacts/contracts/interfaces/**/*.json ./build && del-cli ./build/*.dbg.json &&' +
+      'npx copyfiles -f ./artifacts/contracts/libraries/**/*.json ./build && del-cli ./build/*.dbg.json &&' +
+      'npx copyfiles -f ./artifacts/contracts/test/**/*.json ./build && del-cli ./build/*.dbg.json'
     );
     resolve();
   });
